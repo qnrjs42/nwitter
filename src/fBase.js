575b22832560d5d02c12625619965a1c5a530702
qnrjs42 <qnrjs42@naver.com>
2020-11-15 19:06:08 +0900

    3-1. nweeting

--- src/fBase.js
+++ src/fBase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -18,3 +19,4 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,37 @@
-import React from "react";
+import { dbService } from "fBase";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <>Home</>;
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.collection('nweets').add({
+      nweet: nweet,
+      createdAt: Date.now(),
+    });
+    setNweet('');
+  }
+
+  const onChange = (e) => {
+    const { target: { value } } = e;
+    setNweet(value); 
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </>
+  );
 };
 
 export default Home;
